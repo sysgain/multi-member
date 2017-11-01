80266e9f4b89a41e283f38f1d31db79a6bc061ae
PradeepTS <pradeepts@sysgain.com>
2017-11-01 17:20:03 +0530

    Update configure-geth.sh

--- scripts/configure-geth.sh
+++ scripts/configure-geth.sh
@@ -4,10 +4,11 @@
 # Parameters
 #############
 # Validate that all arguments are supplied
-if [ $# -lt 13 ]; then echo "Insufficient parameters supplied. Exiting"; exit 1; fi
+if [ $# -lt 20 ]; then echo "Insufficient parameters supplied. Exiting"; exit 1; fi
 
 AZUREUSER=$1;
 ARTIFACTS_URL_PREFIX=$4
+NOOFTRIES=3
 
 ###########
 # Constants
@@ -28,7 +29,7 @@ sudo -u $AZUREUSER /bin/bash -c "wget -N ${ARTIFACTS_URL_PREFIX}/scripts/auth-to
 ##################################
 # Initiate loop for error checking
 ##################################
-for LOOPCOUNT in `seq 1 2`; do
+for LOOPCOUNT in `seq 1 $NOOFTRIES`; do
 	sudo -u $AZUREUSER /bin/bash /home/$AZUREUSER/configure-geth-azureuser.sh "$1" "$2" "$3" "$4" "$5" "$6" "$7" "$8" "$9" "${10}" "${11}" "${12}" "${13}" "${14}" "${15}" "${16}" "${17}" "${18}" "${19}" "${20}" "${21}" "${22}" "${23}" "${24}" "${25}" "${26}" "${27}" "${28}" "${29}" "${30}" >> $CONFIG_LOG_FILE_PATH 2>&1;
 	if [ $? -ne 0 ]; then
 		echo "Command failed on try $LOOPCOUNT, retrying..." >> $CONFIG_LOG_FILE_PATH;
@@ -40,5 +41,5 @@ for LOOPCOUNT in `seq 1 2`; do
 	fi
 done
 
-echo "One or more commands failed after 2 tries. Deployment failed." >> $CONFIG_LOG_FILE_PATH;
+echo "One or more commands failed after $LOOPCOUNT tries. Deployment failed." >> $CONFIG_LOG_FILE_PATH;
 exit 1
